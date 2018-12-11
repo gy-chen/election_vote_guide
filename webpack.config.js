576f9fc2d87ddecd454b14d0520abd96f559bad0
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-12-11 11:04:01 +0800

    add sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
+    devtool: "inline-source-map",
     module: {
         rules: [
             {
                 test: /\.ts/,
-                use: 'awesome-typescript-loader',
+                use: 'ts-loader',
                 exclude: /node_modules/
             }
         ]
@@ -15,6 +17,7 @@ module.exports = {
         extensions: ['.ts', '.js']
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new webpack.BannerPlugin('#!/usr/bin/env node')
     ],
     target: "node"
